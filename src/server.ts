dc46f2dddc49effabf480a813a5e088b0bfce077
Hossam Hatem <7ossam7atem1@gmail.com>
2024-08-22 08:10:21 +0300

    fix: improving error handling when socket disconnected

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 const server = net.createServer((socket) => {
   console.log('Client Connected!');
   socket.on('close', (err) => {
-    console.log(`Client disconnected ${err ? 'due to server error.' : '.'}`);
+    console.log(`Client disconnected${err ? ' due to an error.' : '.'}`);
   });
 });
 
